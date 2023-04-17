3a3c8ff1615a6847e1cb68f6e5889d44c3343d8d
kitt-null <130538125+kitt-null@users.noreply.github.com>
2023-04-17 00:02:19 -0700

    upload

---
+++ classes/tasks.class.php
@@ -0,0 +1,16 @@
+<?php
+    class Tasks{
+        public $id
+        public function show(){
+            echo 'show hatdog';
+        }
+        
+        public function insert(){
+            echo 'insert hatdog';
+        }
+
+        public function delete(){
+            echo 'delete hatdog';
+        }
+    }
+?>
